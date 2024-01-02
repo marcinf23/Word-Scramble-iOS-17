677e4195f1d0681dd255a6b88fcd56f64838bfc7
Marcin Frydrych <marcinf23@gmail.com>
2024-01-02 22:50:20 +0100

    Word scramble with challenge

---
+++ 5 WordScramble for iOS 17/__WordScramble_for_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  __WordScramble_for_iOS_17App.swift
+//  5 WordScramble for iOS 17
+//
+//  Created by Marcin Frydrych on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct __WordScramble_for_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
